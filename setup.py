c71c05857ac88ecfe296ffef849391f38bcab2c2
Michael Orr <michael@orr.co>
2015-10-31 10:59:45 -0400

    Support pip versions and pull reqs out of setup.py for reqs.txt

--- setup.py
+++ setup.py
@@ -1,32 +1,59 @@
+import functools
+import pkg_resources
+
 from setuptools import setup, find_packages
+
+from pip.req import parse_requirements as parse_reqs
+
 
 version = '0.2.0'
 
+
+# Compatibility with older versions of pip
+pip_dist = pkg_resources.get_distribution('pip')
+pip_version = tuple(map(int, pip_dist.version.split('.')))
+
+# Use a base partial that will be updated depending on the version of pip
+parse_requirements = functools.partial(parse_reqs, options=None)
+
+if pip_version < (1, 2):
+    # pip versions before 1.2 require an options keyword for using it outside
+    # of invoking a pip shell command
+    from pip.baseparser import parser
+    parse_requirements.keywords['options'] = parser.parse_args()[0]
+
+if pip_version >= (1, 5):
+    # pip 1.5 introduced a session kwarg that is required in later versions
+    from pip.download import PipSession
+    parse_requirements.keywords['session'] = PipSession()
+
+
 setup(name="helga-newrelic",
-      version=version,
-      description=('IRC bot using twisted'),
-      classifiers=['Development Status :: 4 - Beta',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: MIT License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Software Development :: Libraries :: Python Modules'],
-      keywords='irc bot newrelic helga',
-      author='Shaun Duncan',
-      author_email='shaun.duncan@gmail.com',
-      url='https://github.com/shaunduncan/helga',
-      license='MIT',
-      packages=find_packages(),
-      install_requires = [
-          'requests==2.2.1',
-          'lxml>=3',
-      ],
-      entry_points = dict(
-          helga_plugins=[
-              'newrelic = helga_newrelic.plugins:NewRelicStats',
-          ],
-          helga_webhooks=[
-              'newrelic = helga_newrelic.webhooks:newrelic',
-          ],
-      ),
+    version=version,
+    description=('IRC bot using twisted'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords='irc bot newrelic helga',
+    author='Shaun Duncan',
+    author_email='shaun.duncan@gmail.com',
+    url='https://github.com/shaunduncan/helga',
+    license='MIT',
+    packages=find_packages(),
+    install_requires = [
+        str(req.req) for req in parse_requirements('requirements.txt')
+    ],
+    entry_points = dict(
+        helga_plugins=[
+            'newrelic = helga_newrelic.plugins:NewRelicStats',
+        ],
+        helga_webhooks=[
+            'newrelic = helga_newrelic.webhooks:newrelic',
+        ],
+    ),
 )
